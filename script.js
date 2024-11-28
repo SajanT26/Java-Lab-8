c8c6ad63b521d87d2e20d249d812ef0f26c5a5ff
Sajan <n01715615@humber.ca>
2024-11-27 23:40:21 -0500

    made object for student

---
+++ script.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Sajan Thavarasa",
+    age: 20,
+    enrolled: true,
+    courses: ["Javascript", "Web design", "Operating systems", "Database"],
+    displayInfo: function () {
+      return `Student: ${this.name}, Age: ${this.age}`;
+    }
+  };
